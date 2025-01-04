e5d4f8b5619331d3dfb6766d93acc2111b294aac
Bharath-boop <bharathboopathi51550@gmail.com>
2025-01-04 13:57:04 +0530

    update code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./Components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import Signup from "./Components/Signup";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/d" element={<NavBar/>}/>
       </Routes>
     </Router>
   );
