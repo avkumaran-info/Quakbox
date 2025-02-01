3184b5531aec50cd48281ddfd327023db3e6aa16
anitta <anittaarputharaj11@gmail.com>
2025-02-01 14:12:13 +0530

    no message

--- backend/database/migrations/2025_01_26_083437_create_m_videos_table.php
+++ backend/database/migrations/2025_01_26_083437_create_m_videos_table.php
@@ -17,14 +17,9 @@ return new class extends Migration
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('file_path');
+            $table->json('thumbnails')->nullable(); // Store paths to thumbnails as JSON
             $table->integer('views')->default(0);
             $table->timestamps();
-            $table->integer('category_id')->nullable();
-            $table->string('type', 50)->nullable();
-            $table->string('title_size', 50)->nullable();
-            $table->string('title_colour', 50)->nullable();
-            $table->string('defaultthumbnail', 255)->nullable();
-            $table->string('country_code', 10)->nullable();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
