8390ce377c6740b65dba3dfb95b93ca9c54fc641
anitta <anittaarputharaj11@gmail.com>
2025-02-27 15:54:29 +0530

    profile to get

--- backend/app/Models/GeoCountry.php
+++ backend/app/Models/GeoCountry.php
@@ -53,17 +53,30 @@ class GeoCountry extends Model
 
     public static function getCountryComment($cc) {
         return GeoCountry::leftJoin('flag_comments', 'geo_country.code', '=', 'flag_comments.country_code')
-            ->leftJoin('users', 'flag_comments.user_id', '=', 'users.id')
-            ->select(
-                DB::raw('flag_comments.id as comment_id'),
-                DB::raw('users.username as userName'),
-                DB::raw('users.id as userID'),
-                DB::raw('flag_comments.country_code as country_code'),
-                DB::raw('flag_comments.comment as comment'),
-                DB::raw('flag_comments.created_at as created_at')
-            )
-            ->where('geo_country.code', $cc)
-            ->orderBy('flag_comments.created_at', 'DESC') // Order comments by latest first
-            ->get();    
+        ->leftJoin('users', 'flag_comments.user_id', '=', 'users.id')
+        ->select(
+            DB::raw('flag_comments.id as comment_id'),
+            DB::raw('users.username as userName'),
+            DB::raw('users.id as userID'),
+            DB::raw('users.profile_image as userProfilePicture'), // Added profile image
+            DB::raw('flag_comments.country_code as country_code'),
+            DB::raw('flag_comments.comment as comment'),
+            DB::raw('flag_comments.created_at as created_at')
+        )
+        ->where('geo_country.code', $cc)
+        ->orderBy('flag_comments.created_at', 'DESC') // Order comments by latest first
+        ->get()
+        ->map(function ($comment) {
+            return [
+                'comment_id' => $comment->comment_id,
+                'userID' => $comment->userID,
+                'userName' => $comment->userName,
+                'country_code' => $comment->country_code,
+                'comment' => $comment->comment,
+                'created_at' => $comment->created_at,
+                'comment_user_profile_picture' => env('APP_URL') . '/api/images/' . $comment->userProfilePicture // Full profile picture URL
+            ];
+        });
+      
     }
 }
