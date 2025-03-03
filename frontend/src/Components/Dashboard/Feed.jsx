3825fe8b1892d77119157c6a8e7e0b76733a2480
anitta <anittaarputharaj11@gmail.com>
2025-03-03 11:14:17 +0530

    To display Utc time correctly in frontend

--- frontend/src/Components/Dashboard/Feed.jsx
+++ frontend/src/Components/Dashboard/Feed.jsx
@@ -43,9 +43,10 @@ const Feed = ({ countryCode, flag, countryName, handleCountryChange }) => {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   const handleEmojiClick = (emoji) => {
-    setCommentText((prev) => prev + emoji.emoji);
-  };
-
+    const emojiChar = String.fromCodePoint(`0x${emoji.unified}`);
+    setCommentText((prevText) => prevText + emojiChar);
+    console.log(emojiChar);
+  };  
 
   const getCommets = async (post) => {
     try {
@@ -386,20 +387,19 @@ const closeEditCommentPopup = () => {
     const postTime = new Date(timestamp);
     const seconds = Math.floor((now - postTime) / 1000);
 
-    if (seconds < 5) return `Now`;
-    if (seconds < 60) return `${seconds} sec ago`;
+    if (seconds < 60) return "Just now";
     const minutes = Math.floor(seconds / 60);
     if (minutes < 60) return `${minutes} min ago`;
     const hours = Math.floor(minutes / 60);
-    if (hours < 24) return `${hours} hr ago`;
+    if (hours < 24) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
     const days = Math.floor(hours / 24);
-    if (days < 7) return `${days} day ago`;
+    if (days < 7) return `${days} day${days > 1 ? "s" : ""} ago`;
     const weeks = Math.floor(days / 7);
-    if (weeks < 4) return `${weeks} week ago`;
+    if (weeks < 4) return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
     const months = Math.floor(days / 30);
-    if (months < 12) return `${months} month ago`;
+    if (months < 12) return `${months} month${months > 1 ? "s" : ""} ago`;
     const years = Math.floor(days / 365);
-    return `${years} year ago`;
+    return `${years} year${years > 1 ? "s" : ""} ago`;
   };
 
   const handleMessageChange = (event) => {
@@ -812,7 +812,7 @@ const closeEditCommentPopup = () => {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-  
+    
       if (res.data?.status && Array.isArray(res.data.posts)) {
         const formattedPosts = res.data.posts.map((post) => {
           const isLiked = post.likes?.liked_users?.some((user) => user.user_id === currentUserId);
@@ -1498,6 +1498,7 @@ const closeEditCommentPopup = () => {
                               borderRadius: "5px",
                               border: "1px solid #ccc",
                               resize: "none",
+                              fontFamily: `"Noto Color Emoji", sans-serif`
                             }}
                             rows="2"
                             placeholder="Write a comment..."
